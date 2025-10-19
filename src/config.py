415e2f528b102c12fa7852031e1c00e4336368a3
John Friedrich <john.l.friedrich@gmail.com>
2025-10-19 14:35:58 -0600

    Patch src/config.py to fix relative path issues both locally and on GitHub

--- src/config.py
+++ src/config.py
@@ -3,9 +3,12 @@ import yaml
 from pathlib import Path
 
 # Build the path to the config file relative to this file's location
-config_path = Path(__file__).resolve().parent.parent / "src" / "config.yaml"
+# Resolve config.yaml relative to the Git root
+# config_path = Path(__file__).resolve().parent.parent / "src" / "config.yaml"
 #            #parent / "src" / "config.yaml"
 
+repo_root = Path(__file__).resolve().parents[2]
+config_path = repo_root / "src" / "config.yaml"
 
 with open(config_path, "r") as f:
     config = yaml.safe_load(f)
