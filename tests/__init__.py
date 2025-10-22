8c322b27099280b7ba59cb7c0918bafbe6fc2da7
John Friedrich <john.l.friedrich@gmail.com>
2025-10-22 13:36:40 -0600

    Remove legacy config files and CI scaffolding

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-import multiprocessing
-
-try:
-    multiprocessing.set_start_method("spawn", force=True)
-except RuntimeError:
-    # This can happen if set_start_method is called multiple times,
-    # e.g., in interactive environments or when tests are run in a specific way.
-    # We can safely ignore it if it's already set.
-    pass
